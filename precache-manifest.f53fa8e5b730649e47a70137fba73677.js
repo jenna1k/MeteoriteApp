6be39efad7c0fe7e49c363cd369b0c80612be1c7
jenna <jenna_kim1@hotmail.com>
2019-05-24 00:43:51 +0200

    Updates

---
+++ precache-manifest.f53fa8e5b730649e47a70137fba73677.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f14d965173650e3775024e7e3da894",
+    "url": "//MeteoriteApp/index.html"
+  },
+  {
+    "revision": "29c42af54a8834ea32f4",
+    "url": "//MeteoriteApp/static/css/main.f0f15705.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "//MeteoriteApp/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "29c42af54a8834ea32f4",
+    "url": "//MeteoriteApp/static/js/main.7b0c44d2.chunk.js"
+  },
+  {
+    "revision": "a862e87dcf9cfb2edb6e",
+    "url": "//MeteoriteApp/static/js/runtime~main.87fadb96.js"
+  }
+]);
